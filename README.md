38df4f84f5c0621e2dd51e380237f46205838063
Stanley Hillner <shillner@users.noreply.github.com>
2016-04-27 10:27:53 +0200

    initial commit for version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# artifact-spy-plugin
+# Artifact Spy Plugin
+
+## Project Summary
+
+The purpose of this plugin is to spy out the artifacts that are produced by a project build.
+It serializes the project artifact and all attached artifacts into a properties file with the following format:
+
+*   **key:** the artifact's coordinates (_groupId_:_artifactId_:_type_:_version_:_classifier_) 
+*   **value:** the filepath of the artifact relative to the project's base directory
+
+The path of the artifact properties file can be adapted using the parameter `outputFile` and is by default set to `${project.build.directory}/artifact-spy/artifacts.properties`.
+
+
+------------------------------------------------------------
+
+
+## Sample usage
+
+    <project>
+      ...
+      <build>
+        ...
+        <plugins>
+          ...
+          <plugin>
+            <groupId>com.itemis.maven.plugins</groupId>
+            <artifactId>artifact-spy-plugin</artifactId>
+            <version>${version.artifact-spy-plugin}</version>
+            <executions>
+              <execution>
+                <id>spy-artifacts</id>
+                <phase>verify</phase>
+                <goals>
+                  <goal>spy</goal>
+                </goals>
+                <configuration>
+                  <outputFile>${project.build.directory}/spy/artifacts.properties</outputFile>
+                </configuration>
+              </execution>
+            </executions>
+          </plugin>
+        </plugins>
+      </build>
+    </project>
+
+
+------------------------------------------------------------
+
+
+## Sample output
+
+
+### Project with packaging 'pom'
+
+    #
+    #Wed Apr 27 09:26:52 CEST 2016
+    com.itemis.maven.plugins\:org-parent\:pom\:1=pom.xml
+
+
+### Project with packaging 'jar'
+
+    #
+    #Wed Apr 27 08:51:13 CEST 2016
+    com.itemis.maven.plugins\:cdi-plugin-utils\:jar\:1.0.0=target/cdi-plugin-utils-1.0.0.jar
+    
+### Project attaching an additional artifact
+
+    #
+    #Wed Apr 27 09:08:03 CEST 2016
+    com.itemis.maven.plugins\:ump-it-core\:zip\:pack\:0.0.1=target/ump-it-core-resources-0.0.1.zip
+    com.itemis.maven.plugins\:ump-it-core\:jar\:0.0.1=target/ump-it-core-0.0.1.jar
+    
---
+++ src/main/java/com/itemis/maven/plugins/artifact/spy/ArtifactSpyMojo.java
@@ -0,0 +1,79 @@
+package com.itemis.maven.plugins.artifact.spy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * A small Mojo that only detects the artifacts produced by the build of the current project and writes them out to a
+ * properties file.<br>
+ * The Mojo should be bound to a late build phase in order to detect all artifacts produced by the build. By default it
+ * is bound to the verify phase.<br>
+ * <br>
+ *
+ * The properties will have the following format:
+ * <ul>
+ * <li><b>key: </b>artifact coordinates</li>
+ * <li><b>value: </b>filepath of the artifact (relative to the project's base directory)</li>
+ * </ul>
+ *
+ * @author <a href="mailto:stanley.hillner@itemis.de">Stanley Hillner</a>
+ * @since 1.0.0
+ */
+@Mojo(name = "spy", defaultPhase = LifecyclePhase.VERIFY)
+public class ArtifactSpyMojo extends AbstractMojo {
+  @Component
+  private MavenProject project;
+
+  /**
+   * The path of the properties file which will then contain all artifacts that have been produced by the build of the
+   * current project.
+   */
+  @Parameter(required = true, defaultValue = "${project.build.directory}/artifact-spy/artifacts.properties")
+  private File outputFile;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    Properties props = new Properties();
+    Artifact projectArtifact = this.project.getArtifact();
+    if ("pom".equals(projectArtifact.getType())) {
+      projectArtifact.setFile(this.project.getFile());
+    }
+    props.setProperty(projectArtifact.toString(), getProjectRelativePath(projectArtifact.getFile()));
+
+    for (Artifact a : this.project.getAttachedArtifacts()) {
+      props.setProperty(a.toString(), getProjectRelativePath(a.getFile()));
+    }
+
+    FileOutputStream fos = null;
+    try {
+      this.outputFile.getParentFile().mkdirs();
+      fos = new FileOutputStream(this.outputFile);
+      props.store(fos, "");
+    } catch (IOException e) {
+      throw new MojoFailureException(
+          "Error serializing the project artifacts to file: " + this.outputFile.getAbsolutePath(), e);
+    } finally {
+      try {
+        fos.close();
+      } catch (IOException e) {
+        throw new MojoExecutionException("Could not close output stream after writing project artifacts to file: "
+            + this.outputFile.getAbsolutePath(), e);
+      }
+    }
+  }
+
+  private String getProjectRelativePath(File f) {
+    return this.project.getBasedir().toURI().relativize(f.toURI()).getPath();
+  }
+}
